61a7b81ac56532baa8326069e8cc58abcfad4707
Emanjo <emil.jona@gmail.com>
2019-03-23 11:26:05 +0100

    Some server config

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 const discord = require('discord.js');
 const client = new discord.Client();
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Contet-Type', 'text/html');
+	res.end('<p>Joke Machine Bot is running!</p>');
+});
+
+server.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
+
+
 require('dotenv').config();
 
 client.on('message', msg => {
